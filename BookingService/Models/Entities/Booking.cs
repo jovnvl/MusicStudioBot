82416e7f880c7ae7927d89df0977758c2f324145
Zhovnerchuk <zhovnerchuk@electronxray.com>
2026-02-18 17:26:07 +0300

    Создание проекта для микросервиса Booking

---
+++ BookingService/Models/Entities/Booking.cs
@@ -0,0 +1,19 @@
+﻿namespace BookingService.Models.Entities
+{
+    public class Booking
+    {
+        public int Id { get; }
+        public int UserId { get; }
+        public int RoomId { get; }       
+        public DateTime CreationDate { get; }
+        public int? Status { get; }
+        public Booking(int id, int userId, int roomId, DateTime creationDate, int? status)
+        {
+            Id = id;
+            UserId  = userId;
+            RoomId = roomId;
+            CreationDate = creationDate;
+            Status = status;
+        }
+    }
+}
