a150a17cf19260a1d1f70de28df175745956fe96
Mikhail Belkin <m.beleffa@gmail.com>
2026-04-08 14:04:09 +0300

    Добавление CORS в сервисы для поддержки web-запросов

--- IdentityService/Program.cs
+++ IdentityService/Program.cs
@@ -32,7 +32,15 @@ builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
 var app = builder.Build();
+
+app.UseCors();
 
 if (app.Environment.IsDevelopment())
 {
