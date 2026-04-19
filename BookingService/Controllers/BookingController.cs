b64ec11ffb108f62b9d142cac3d57ff276aa8d10
Mikhail Belkin <m.beleffa@gmail.com>
2026-04-19 18:38:42 +0300

    Добавление публикации событий в Rabbit MQ для Booking Service

--- BookingService/Controllers/BookingController.cs
+++ BookingService/Controllers/BookingController.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using BookingService.DTO;
+﻿using BookingService.DTO;
 using BookingService.Models.Entities;
 using BookingService.Services;
+using BookingService.Services.RabbitMQ;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookingService.Controllers
 {
@@ -11,9 +12,16 @@ namespace BookingService.Controllers
     public class BookingController : ControllerBase
     {
         private readonly IBookingService _bookingService;
-        public BookingController(IBookingService bookingService)
+        private readonly IRabbitMQPublisher _rabbitMQPublisher;
+        public BookingController(IBookingService bookingService, IRabbitMQPublisher rabbitMQPublisher)
         {
             _bookingService = bookingService;
+            _rabbitMQPublisher = rabbitMQPublisher;
+        }
+
+        private async Task LogToServiceAsync(string level, string eventType, string message)
+        {
+            await _rabbitMQPublisher.PublishAsync("logging_service_queue", new LogEventDto(level, eventType, message));
         }
 
         // GET: api/booking
@@ -28,12 +36,13 @@ namespace BookingService.Controllers
             
             catch (OperationCanceledException)
             {
+                await LogToServiceAsync("Error", "get-bookings-operation-canceled", "Get bookings operation canceled");
                 return StatusCode(499); 
             }
             
             catch (Exception ex)
             {
-                // Добавить логирование ошибки
+                await LogToServiceAsync("Error", "get-bookings-internal-server-error", "Get bookings internal server error");
 
                 return StatusCode(500, new
                 {
@@ -52,6 +61,7 @@ namespace BookingService.Controllers
                 var booking = await _bookingService.GetBookingByIdAsync(id, ct);
                 if (booking == null)
                 {
+                    await LogToServiceAsync("Error", "booking-not-found", $"Booking with ID {id} not found");
                     return NotFound(new { Message = $"Бронь с ID {id} не найдена" });
                 }
                 return Ok(booking);
@@ -64,7 +74,7 @@ namespace BookingService.Controllers
             
             catch (Exception ex)
             {
-                // Добавить логирование ошибки
+                await LogToServiceAsync("Error", "get-booking-internal-server-error", "Get booking internal server error");
 
                 return StatusCode(500, new
                 {
@@ -80,14 +90,19 @@ namespace BookingService.Controllers
         public async Task<ActionResult<Booking>> CreateBookingAsync(BookingDto bookingDto, CancellationToken ct = default)
         {
             if (!ModelState.IsValid)
+            {
+                await LogToServiceAsync("Error", "create-booking-validation-problem", "Validation problem");
                 return ValidationProblem(ModelState);
+            }
             try
             {
                 var createdBooking = await _bookingService.CreateBookingAsync(bookingDto, ct);
+                await LogToServiceAsync("Information", "create-booking", $"New booking {createdBooking.Id} created");
                 return CreatedAtRoute("GetBookingAsync", new { id = createdBooking.Id }, createdBooking);
             }
             catch (InvalidOperationException ex)
             {
+                await LogToServiceAsync("Error", "create-booking-invalid-operation", "Invalid operation");
                 return BadRequest(new { Message = ex.Message });
             }
             catch (OperationCanceledException)
@@ -96,7 +111,7 @@ namespace BookingService.Controllers
             }
             catch (Exception ex)
             {
-                // TODO: Логирование
+                await LogToServiceAsync("Error", "create-booking-internal-server-error", "Create booking internal server error");
                 return StatusCode(500, new { Message = $"Внутренняя ошибка сервера: {ex}" });
             }
         }
@@ -107,29 +122,26 @@ namespace BookingService.Controllers
         {
             try
             {
-                //var exists = await _bookingsService.ExistsBookingAsync(id, ct);
-                //if (!exists)
-                //{
-                //    return NotFound(new { Message = $"Кабинет с ID {id} не найден" });
-                //}
-
                 var deleted = await _bookingService.DeleteBookingAsync(id, ct);
                 if (!deleted)
                 {
-                    return BadRequest(new { Message = "Не удалось удалить бронь на кaбанет" });
+                    await LogToServiceAsync("Error", "delete-booking-error", "Booking was not deleted");
+                    return BadRequest(new { Message = "Не удалось удалить бронь на кaбинет" });
                 }
 
+                await LogToServiceAsync("Information", "delete-booking", $"Booking {id} was deleted");
                 return Ok();
             }
 
             catch (OperationCanceledException)
             {
+                await LogToServiceAsync("Error", "delete-booking-operation-canceled", "Delete booking operation canceled");
                 return StatusCode(499);
             }
 
             catch (Exception ex)
             {
-                // Добавить логирование ошибки
+                await LogToServiceAsync("Error", "delete-booking-internal-server-error", "Delete booking internal server error");
 
                 return StatusCode(500, new
                 {
---
+++ BookingService/DTO/LogEventDto.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace BookingService.DTO
+{
+    public class LogEventDto
+    {
+        public DateTime Timestamp { get; private set; } = DateTime.UtcNow;
+        public string Service { get; private set; } = "booking-service";
+        public string Level { get; private set; }
+        public string EventType { get; private set; }
+        public string Message { get; private set; } 
+
+        public LogEventDto(string level, string eventType, string message   )
+        {
+            Level = level; 
+            EventType = eventType;
+            Message = message;
+        }
+    }
+    
+    
+}
