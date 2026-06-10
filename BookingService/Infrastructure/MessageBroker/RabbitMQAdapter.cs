1fae88a9bb87ee3d20d9b140afd025bb70249fcd
Jovnvl <Jovn@yandex.ru>
2026-06-10 22:33:26 +0300

    Booking - RabbitMqAdapter исправлен на работу с подтверждением издателя и с блокировкой SemaphoreSlim  на 1 соединение и на 1 канал (было 1 блокировка и на соединие и на канал)

--- BookingService/Infrastructure/MessageBroker/RabbitMQAdapter.cs
+++ BookingService/Infrastructure/MessageBroker/RabbitMQAdapter.cs
@@ -14,6 +14,7 @@ namespace BookingService.Infrastructure.MessageBroker
         private IConnection? _connection;
         private IChannel? _channel;
 
+        private readonly SemaphoreSlim _connectLock = new(1, 1);
         private readonly SemaphoreSlim _publishLock = new(1, 1);
 
         public RabbitMQAdapter(ILogger<RabbitMQAdapter> logger, RabbitMqConnection rabbitMqConnection, IConfiguration configuration)
@@ -31,18 +32,25 @@ namespace BookingService.Infrastructure.MessageBroker
                 return;
 
             // Синхронизация на установку соединения
-            await _publishLock.WaitAsync(ct);
+            await _connectLock.WaitAsync(ct);
             try
             {
                 if (_connection != null && _channel != null && _connection.IsOpen && _channel.IsOpen)
                     return;
 
                 _connection = await _factory.CreateConnectionAsync(ct);
-                _channel = await _connection.CreateChannelAsync(cancellationToken: ct);
+                _channel = await _connection.CreateChannelAsync(
+                //Для RabbitMQ.Client 7.x нужно создавать канал с включенными publisher confirmations:
+                //иначе публикация не использует Publisher Confirms - создает обычный канал без подтверждений издателя.
+                    new CreateChannelOptions(
+                        publisherConfirmationsEnabled: true,
+                        publisherConfirmationTrackingEnabled: true),
+
+                        cancellationToken: ct);
             }
             finally
             {
-                _publishLock.Release();
+                _connectLock.Release();
             }
         }
 
@@ -58,13 +66,14 @@ namespace BookingService.Infrastructure.MessageBroker
                 {
                     if (_channel == null)
                         throw new InvalidOperationException("RabbitMQ channel is not initialized.");
-
+                    /*Очередь создается/вызывается для каждого сообщения. Это лишняя операция.
                     await _channel.QueueDeclareAsync(
                         queue: queueName,
-                        durable: false,
+                        durable: true,
                         exclusive: false,
                         autoDelete: false,
                         cancellationToken: ct);
+                    */
 
                     var options = new JsonSerializerOptions
                     {
@@ -75,9 +84,17 @@ namespace BookingService.Infrastructure.MessageBroker
                     var message = JsonSerializer.Serialize(sendMessage, options);
                     var body = Encoding.UTF8.GetBytes(message);
 
+                    var properties = new BasicProperties
+                    {
+                        Persistent = true
+                    };
                     await _channel.BasicPublishAsync(
                         exchange: "",
                         routingKey: queueName,
+                        //гарантирует только, что сообщение не потеряется при отсутствии маршрута.То есть если нет подходящей очереди, брокер вернет сообщение издателю.
+                        mandatory: false,
+                        //Тогда: при включении(выше в CreateChannelAsync()) new CreateChannelOptions(..) будет ожидать подтверждения от брокера.
+                        basicProperties: properties,
                         body: body,
                         cancellationToken: ct
                         );
